dd76f437c96dac0e54eea359491852d8d7d33b7d
lorenzosalti <lorenzosalti@gmail.com>
2025-04-10 16:03:15 +0200

    feat: movies title and genre display in page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import movies from './data/movies.json'
 
 
 
@@ -7,7 +7,14 @@ function App() {
 
   return (
     <>
+      <h1>Lista dei film</h1>
 
+      <ul>
+        {movies.map((movie, i) => <li key={i}>
+          <h3>{movie.title}</h3>
+          <h5>Genere: {movie.genre}</h5>
+        </li>)}
+      </ul>
     </>
   )
 }
