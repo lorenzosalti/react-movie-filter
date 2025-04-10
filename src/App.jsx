69d7c955be9ab7ad8f1dd0bffcff87e858f1b057
lorenzosalti <lorenzosalti@gmail.com>
2025-04-10 17:16:02 +0200

    feat: useEffect implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-import movies from './data/movies.json'
+// data
+import dataMovies from './data/movies.json'
 
+// hooks
+import { useEffect, useState } from 'react'
 
 
 function App() {
+
+  const [movies, setMovies] = useState(dataMovies)
+
+  const [filter, setFilter] = useState(false)
+
+  useEffect(() => {
+    console.log('componente montato')
+
+    let filteredMovies = movies
+
+    if (filter) {
+      filteredMovies = filteredMovies.filter(movie => movie.genre === "Azione")
+    }
+
+    setMovies(filteredMovies)
+
+  }, [filter])
 
 
   return (
     <>
       <h1>Lista dei film</h1>
+
+      <button onClick={() => setFilter(prev => !prev)}>filtra</button>
 
       <ul>
         {movies.map((movie, i) => <li key={i}>
